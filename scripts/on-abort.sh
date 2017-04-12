8a3861ae165e516fdceee3f4035a5d38803b30bc
Guillaume Paran <guillaume.paran@inria.fr>
2017-04-12 17:49:52 +0200

    REMOVE trap-abort.sh and use on-abort.sh instead
    on-abort.sh is triggered by Jenkins job (post-build task).

--- scripts/on-abort.sh
+++ scripts/on-abort.sh
@@ -1,14 +1,15 @@
-#!/bin/bash -ex
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. "$SCRIPT_DIR"/dashboard.sh
+#!/bin/bash
+echo "------------- ON-ABORT SCRIPT -------------"
 
 COMPILER="$1"
 ARCHITECTURE="$2"
 BUILD_TYPE="$3"
 BUILD_OPTIONS="${*:4}"
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. "$SCRIPT_DIR"/dashboard.sh
+
 # We need dashboard env vars in case of abort
 dashboard-export-vars "$COMPILER" "$ARCHITECTURE" "$BUILD_TYPE" "$BUILD_OPTIONS"
 
-echo "--------- ABORT TRAPPED ---------"
 dashboard-notify "status=aborted"
