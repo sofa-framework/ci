5c78b011e378e710b812d257b98ccf94a0f75c1b
Guillaume Paran <guillaume.paran@inria.fr>
2017-05-04 16:27:22 +0200

    FIX github-notify false "sent" status

--- scripts/github.sh
+++ scripts/github.sh
@@ -9,29 +9,31 @@ github-notify() {
 
     local options="$-"
     local notify="not sent"
+    local response=""
 
     set +x # Private stuff here: echo disabled
-    if [ -n "$GITHUB_CONTEXT" ] &&
+    if [[ "$GITHUB_NOTIFY" == "true" ]] &&
+    [ -n "$GITHUB_CONTEXT" ] &&
     [ -n "$GITHUB_TARGET_URL" ] &&
     [ -n "$GITHUB_REPOSITORY" ] &&
     [ -n "$GITHUB_COMMIT_HASH" ] &&
     [ -n "$GITHUB_SOFABOT_TOKEN" ]; then
-        if [[ "$GITHUB_NOTIFY" == "true" ]]; then
-            local request="{
-                \"context\": \"$GITHUB_CONTEXT\",
-                \"state\": \"$state\",
-                \"description\": \"$message\",
-                \"target_url\": \"$GITHUB_TARGET_URL\"
-            }"
+        local request="{
+            \"context\": \"$GITHUB_CONTEXT\",
+            \"state\": \"$state\",
+            \"description\": \"$message\",
+            \"target_url\": \"$GITHUB_TARGET_URL\"
+        }"
 
-            response="$(curl --silent --header "Authorization: token $GITHUB_TOKEN"  --data "$request" "https://api.github.com/repos/$GITHUB_REPOSITORY/statuses/$GITHUB_COMMIT_HASH")"
+        response="$(curl --silent --header "Authorization: token $GITHUB_TOKEN"  --data "$request" "https://api.github.com/repos/$GITHUB_REPOSITORY/statuses/$GITHUB_COMMIT_HASH")"
+        if [ -n "$response" ]; then
+            notify="sent"
         fi
-        notify="sent"
     fi
     set -$options
 
-    echo "Notify GitHub ($notify): $GITHUB_CONTEXT: $message"
-    if [[ "$notify" == "sent" ]]; then
+    echo "Notify GitHub ($notify): [$state] $GITHUB_CONTEXT - $message"
+    if [ -n "$response" ]; then
         echo "GitHub reponse: $response"
     fi
 }
