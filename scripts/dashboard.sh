90fb49ac5901e0ebb3faff2895d8d71359dad71f
Guillaume Paran <guillaume.paran@inria.fr>
2017-05-12 15:07:41 +0200

    EDIT: dashboard-init does not notify "queued" anymore

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -12,6 +12,10 @@ dashboard-notify-explicit() {
         message="$message&$1"; shift
     done
 
+    if [[ "$DASH_FULLBUILD" == "true" ]]; then
+        message="fullbuild=true&$message"
+    fi
+
     if [[ "$DASH_NOTIFY" == "true" ]] && [ -n "$DASH_DASHBOARD_URL" ]; then
         # wget --no-check-certificate --no-verbose --output-document=/dev/null --post-data="$message" "$DASH_DASHBOARD_URL"
         # curl --request POST "$DASH_DASHBOARD_URL" --data="$message"
@@ -22,20 +26,12 @@ dashboard-notify-explicit() {
 }
 
 dashboard-notify() {
-    dashboard-notify-explicit "sha=$DASH_COMMIT_HASH" "config=$DASH_CONFIG" $*
+    dashboard-notify-explicit "sha=$DASH_COMMIT_HASH" "config=$DASH_CONFIG" "job_url=$JOB_URL" "build_url=$BUILD_URL" $*
 }
 
 dashboard-init() {
     echo "DASH: Create/update commit line"
     dashboard-notify-explicit "sha=$DASH_COMMIT_HASH" "comment=$DASH_COMMIT_SUBJECT" "date=$DASH_COMMIT_DATE" "author=$DASH_COMMIT_AUTHOR" "branch=$DASH_COMMIT_BRANCH"
-
-    # TODO: status=queue
-    echo "DASH: Create/update build frame with empty status"
-    if [[ "$DASH_FULLBUILD" == "true" ]]; then
-        dashboard-notify "build_url=$BUILD_URL" "job_url=$JOB_URL" "status=" "fullbuild=true"
-    else
-        dashboard-notify "build_url=$BUILD_URL" "job_url=$JOB_URL" "status="
-    fi
 }
 
 dashboard-get-config() {
