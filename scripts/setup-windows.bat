a84874916917265b232f3e039a0d2c7e9eabaee9
Guillaume Paran <guillaume.paran@sofa-framework.org>
2020-03-02 18:04:05 +0100

    ADD assimp, CGAL, OpenCascade to Windows env

--- scripts/setup-windows.bat
+++ scripts/setup-windows.bat
@@ -30,10 +30,7 @@ call refreshenv && echo OK
 
 REM Install plugins dependencies
 choco install -y cuda --version=10.2.89.20191206
-REM Assimp: source code to build: https://github.com/assimp/assimp/releases
 REM Bullet: source code to build: https://github.com/bulletphysics/bullet3/releases
-REM OpenCascade: installer: https://www.opencascade.com/content/latest-release
-REM CGAL: installer: https://github.com/CGAL/cgal/releases
 REM Pybind: source code to build: https://github.com/pybind/pybind11/releases
 call refreshenv && echo OK
 
@@ -78,10 +75,10 @@ call refreshenv && echo OK
 
 
 REM Install Qt
-set QT_VERSION_MAJOR=5
-set QT_VERSION_MINOR=12
-set QT_VERSION_PATCH=6
-REM setx QTDIR "C:\Qt\%QT_VERSION_MAJOR%.%QT_VERSION_MINOR%.%QT_VERSION_PATCH%\msvc2017_64"
+set QT_MAJOR=5
+set QT_MINOR=12
+set QT_PATCH=6
+REM setx QTDIR "C:\Qt\%QT_MAJOR%.%QT_MINOR%.%QT_PATCH%\msvc2017_64"
 REM setx QTDIR64 %QTDIR%
 REM setx Qt5_DIR %QTDIR%
 call refreshenv && echo OK
@@ -89,24 +86,57 @@ if not exist "%APPDATA%\Qt\" mkdir %APPDATA%\Qt
 powershell Invoke-WebRequest https://raw.githubusercontent.com/guparan/DockerSofaBuilder/master/sofabuilder_windows_vs2017/qtaccount.ini -OutFile %APPDATA%\Qt\qtaccount.ini
 powershell Invoke-WebRequest https://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe -OutFile %WORKDIR%\qtinstaller.exe
 powershell Invoke-WebRequest https://raw.githubusercontent.com/guparan/DockerSofaBuilder/master/sofabuilder_windows_vs2017/qtinstaller_controlscript_template.qs -OutFile %WORKDIR%\qtinstaller_controlscript_template.qs
-powershell -Command "(gc %WORKDIR%\qtinstaller_controlscript_template.qs) -replace '_QTVERSION_', %QT_VERSION_MAJOR%%QT_VERSION_MINOR%%QT_VERSION_PATCH% | Out-File -encoding ASCII %WORKDIR%\qtinstaller_controlscript.qs"
+powershell -Command "(gc %WORKDIR%\qtinstaller_controlscript_template.qs) -replace '_QTVERSION_', %QT_MAJOR%%QT_MINOR%%QT_PATCH% | Out-File -encoding ASCII %WORKDIR%\qtinstaller_controlscript.qs"
 %WORKDIR%\qtinstaller.exe --script %WORKDIR%\qtinstaller_controlscript.qs
 
 
 REM Install Boost
-set BOOST_VERSION_MAJOR=1
-set BOOST_VERSION_MINOR=69
-set BOOST_VERSION_PATCH=0
+set BOOST_MAJOR=1
+set BOOST_MINOR=69
+set BOOST_PATCH=0
 call refreshenv && echo OK
 powershell Invoke-WebRequest https://raw.githubusercontent.com/guparan/DockerSofaBuilder/master/sofabuilder_windows_vs2017/wait_process_to_end.bat -OutFile %WORKDIR%\wait_process_to_end.bat
-powershell Invoke-WebRequest https://boost.teeks99.com/bin/%BOOST_VERSION_MAJOR%.%BOOST_VERSION_MINOR%.%BOOST_VERSION_PATCH%/boost_%BOOST_VERSION_MAJOR%_%BOOST_VERSION_MINOR%_%BOOST_VERSION_PATCH%-msvc-14.1-64.exe -OutFile %WORKDIR%\boostinstaller.exe
+powershell Invoke-WebRequest https://boost.teeks99.com/bin/%BOOST_MAJOR%.%BOOST_MINOR%.%BOOST_PATCH%/boost_%BOOST_MAJOR%_%BOOST_MINOR%_%BOOST_PATCH%-msvc-14.1-64.exe -OutFile %WORKDIR%\boostinstaller.exe
 %WORKDIR%\boostinstaller.exe /NORESTART /VERYSILENT /DIR=C:\boost & %WORKDIR%\wait_process_to_end.bat "boostinstaller.exe"
+
+
+REM Install Assimp
+set ASSIMP_MAJOR=4
+set ASSIMP_MINOR=1
+set ASSIMP_PATCH=0
+call refreshenv && echo OK
+powershell Invoke-WebRequest https://raw.githubusercontent.com/guparan/DockerSofaBuilder/master/sofabuilder_windows_vs2017/wait_process_to_end.bat -OutFile %WORKDIR%\wait_process_to_end.bat
+powershell Invoke-WebRequest https://github.com/assimp/assimp/releases/download/v%ASSIMP_MAJOR%.%ASSIMP_MINOR%.%ASSIMP_PATCH%/assimp-sdk-%ASSIMP_MAJOR%.%ASSIMP_MINOR%.%ASSIMP_PATCH%-setup.exe -OutFile %WORKDIR%\assimpinstaller.exe
+%WORKDIR%\assimpinstaller.exe /NORESTART /VERYSILENT /DIR=C:\assimp & %WORKDIR%\wait_process_to_end.bat "assimpinstaller.exe"
+set AddToUserPATH=%AddToUserPATH%;C:\assimp
+
+
+REM Install CGAL
+set CGAL_MAJOR=5
+set CGAL_MINOR=0
+set CGAL_PATCH=2
+call refreshenv && echo OK
+powershell Invoke-WebRequest https://raw.githubusercontent.com/guparan/DockerSofaBuilder/master/sofabuilder_windows_vs2017/wait_process_to_end.bat -OutFile %WORKDIR%\wait_process_to_end.bat
+powershell Invoke-WebRequest https://github.com/CGAL/cgal/releases/download/releases/CGAL-%CGAL_MAJOR%.%CGAL_MINOR%.%CGAL_PATCH%/CGAL-%CGAL_MAJOR%.%CGAL_MINOR%.%CGAL_PATCH%-Setup.exe -OutFile %WORKDIR%\cgalinstaller.exe
+%WORKDIR%\cgalinstaller.exe /S /D=C:\CGAL & %WORKDIR%\wait_process_to_end.bat "cgalinstaller.exe"
+set AddToUserPATH=%AddToUserPATH%;C:\CGAL
+
+
+REM Install OpenCascade
+set OCC_MAJOR=7
+set OCC_MINOR=4
+set OCC_PATCH=0
+call refreshenv && echo OK
+powershell Invoke-WebRequest https://raw.githubusercontent.com/guparan/DockerSofaBuilder/master/sofabuilder_windows_vs2017/wait_process_to_end.bat -OutFile %WORKDIR%\wait_process_to_end.bat
+powershell Invoke-WebRequest http://transfer.sofa-framework.org/opencascade-%OCC_MAJOR%.%OCC_MINOR%.%OCC_PATCH%-vc14-64.exe -OutFile %WORKDIR%\occinstaller.exe
+%WORKDIR%\occinstaller.exe /NORESTART /VERYSILENT /DIR=C:\OpenCascade & %WORKDIR%\wait_process_to_end.bat "occinstaller.exe"
+set AddToUserPATH=%AddToUserPATH%;C:\OpenCascade\opencascade-%OCC_MAJOR%.%OCC_MINOR%.%OCC_PATCH%
 
 
 REM Finalize environment
 call refreshenv && echo OK
 setx PYTHONIOENCODING UTF-8
-REM set AddToUserPATH="%AddToUserPATH%;C:\Qt\%QT_VERSION_MAJOR%.%QT_VERSION_MINOR%.%QT_VERSION_PATCH%\msvc2017_64\bin"
+REM set AddToUserPATH="%AddToUserPATH%;C:\Qt\%QT_MAJOR%.%QT_MINOR%.%QT_PATCH%\msvc2017_64\bin"
 REM set AddToUserPATH="%AddToUserPATH%;C:\boost"
 REM set AddToUserPATH="%AddToUserPATH%;C:\boost\lib64-msvc-14.1"
 set AddToUserPATH=%AddToUserPATH%;C:\Program Files\Git\bin
