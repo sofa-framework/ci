0aad496df1bbdbf7b13daf2b1f207b35bff10b40
Guillaume Paran <guillaume.paran@sofa-framework.org>
2020-03-23 17:59:34 +0100

    FIX Eigen3 detection on Windows (4)

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -182,9 +182,8 @@ if vm-is-windows; then # Finding libs on Windows
         add-cmake-option "-DPYTHON_INCLUDE_DIR=$VM_PYTHON_PATH/include"
     fi
     if [ -d "$VM_EIGEN3_PATH" ]; then
-        # Create EIGEN3_ROOT as env var and CMake var
         export EIGEN3_ROOT_DIR="$VM_EIGEN3_PATH"
-        add-cmake-option "-DEIGEN3_ROOT=$VM_EIGEN3_PATH"
+        # add-cmake-option "-DEIGEN3_ROOT=$VM_EIGEN3_PATH"
     fi
 fi
 if vm-is-macos; then
