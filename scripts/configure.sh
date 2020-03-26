1a0bc22bb1ff9b4b79b139a5c5247787105b93d5
Guillaume Paran <guillaume.paran@sofa-framework.org>
2020-03-26 07:57:27 +0100

    [CentOS] Re-enable CGAL build test

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -295,10 +295,6 @@ else # This is not a "package" build
         fi
         if [[ "$VM_HAS_CGAL" == "true" ]]; then
             if [ -d "$VM_CGAL_PATH" ]; then
-                if vm-is-centos; then
-                    # Disable CGAL build test (see FindCGAL.cmake)
-                    add-cmake-option "-DCGAL_TEST_RUNS=TRUE"
-                fi
                 add-cmake-option "-DCGAL_DIR=$VM_CGAL_PATH"
             fi
             add-cmake-option "-DPLUGIN_CGALPLUGIN=ON"
