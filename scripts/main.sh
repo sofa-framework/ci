70477cde496c36965175024b038574db99d9d35f
Guillaume Paran <guillaume.paran@sofa-framework.org>
2020-09-11 14:43:42 +0200

    [scripts] Update wikipedia URL for MacOS compiler

--- scripts/main.sh
+++ scripts/main.sh
@@ -130,7 +130,7 @@ elif vm-is-macos; then
     echo "AppleClang version: $(clang --version | grep -o clang-[^\)]*)"
     echo "AppleClang install dir: $(clang --version | grep InstalledDir)"
 
-    echo "AppleClang/Clang correspondance: https://en.wikipedia.org/wiki/Xcode#Xcode_7.0_-_11.x_%28since_Free_On-Device_Development%29"
+    echo "AppleClang/Clang correspondance: https://en.wikipedia.org/wiki/Xcode#Xcode_7.0_-_12.x_%28since_Free_On-Device_Development%29"
     echo "Example: AppleClang 1001.0.46.3 is based on Clang 7.0.0"
 
     if [ -x "$(command -v xcodebuild)" ]; then
