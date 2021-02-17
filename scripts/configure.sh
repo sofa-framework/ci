b328caee1d338056398cdb8a53fa7d180d2dde19
Guillaume Paran <guillaume.paran@sofa-framework.org>
2021-02-17 10:10:12 +0100

    [scripts] ADD python logs

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -201,6 +201,10 @@ else
         python2_lib="$(find $python2_path/lib64 -name libpython2*.so -maxdepth 1 -type f)"
         python2_include="$(find $python2_path/include -name python2* -maxdepth 1 -type d)"
         python2_exec="$(which python2)"
+        echo "python2_path = $python2_path"
+        echo "python2_lib = $python2_lib"
+        echo "python2_include = $python2_include"
+        echo "python2_exec = $python2_exec"
         if [ -e "$python2_lib" ] && [ -e "$python2_include" ] && [ -e "$python2_exec" ]; then
             add-cmake-option "-DPYTHON_LIBRARY=$python2_lib"
             add-cmake-option "-DPYTHON_INCLUDE_DIR=$python2_include"
@@ -213,6 +217,10 @@ else
         python3_lib="$(find $python3_path/lib64 -name libpython3*.so -maxdepth 1 -type f)"
         python3_include="$(find $python3_path/include -name python3* -maxdepth 1 -type d)"
         python3_exec="$(which python3)"
+        echo "python3_path = $python3_path"
+        echo "python3_lib = $python3_lib"
+        echo "python3_include = $python3_include"
+        echo "python3_exec = $python3_exec"
         if [ -e "$python3_lib" ] && [ -e "$python3_include" ] && [ -e "$python3_exec" ]; then
             add-cmake-option "-DPython_LIBRARY=$python3_lib"
             add-cmake-option "-DPython_INCLUDE_DIR=$python3_include"
