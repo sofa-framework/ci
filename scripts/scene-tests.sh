4058f997eec57e27c2bfd2a1b7b78c961188a57a
Guillaume Paran <guillaume.paran@sofa-framework.org>
2021-03-09 11:30:56 +0100

    Try to fix export-to-junit-xml (2)

--- scripts/scene-tests.sh
+++ scripts/scene-tests.sh
@@ -650,19 +650,19 @@ export-to-junit-xml() {
         <testcase name="'$scene_name'" type_param="" status="run" time="'$elapsed_sec'" classname="SceneTests.'$scene_path'">'
 
         while read crash_msg; do
-            crash_msg_short="$(echo "$crash_msg" | sed 's#^[^: ]*: ##')"
+            crash_msg_short="$(echo $crash_msg | sed 's#^[^: ]*: ##' | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g;')"
             success="false"
             echo '
-            <error message="<![CDATA['"$crash_msg_short"' ]]>">
+            <error message="'$crash_msg_short'">
 <![CDATA['"$(cat $output_dir/$scene/output.txt || echo "export-to-junit-xml: error while running \"cat $output_dir/$scene/output.txt\". See logs for details.")"' ]]>
             </error>'
         done < <( grep -o "${scene}.*" "$output_dir/reports/crashes.txt" )
 
         while read error_msg; do
-            error_msg_short="$(echo "$crash_msg" | sed 's#^[^: ]*: ##')"
+            error_msg_short="$(echo $crash_msg | sed 's#^[^: ]*: ##' | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g;')"
             success="false"
             echo '
-            <failure message="<![CDATA['"$error_msg_short"' ]]>">
+            <failure message="'$error_msg_short'">
 <![CDATA['"$(cat $output_dir/$scene/output.txt || echo "export-to-junit-xml: error while running \"cat $output_dir/$scene/output.txt\". See logs for details.")"' ]]>
             </failure>'
         done < <( grep -o "${scene}.*" "$output_dir/reports/errors.txt" )
