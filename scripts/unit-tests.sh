ecdf9e5d4304b9aaaf45e1c0be8aa324c5946b8a
Guillaume Paran <guillaume.paran@sofa-framework.org>
2021-04-20 10:15:01 +0200

    [scripts] FIX broken XML reports on MacOS (2)

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -134,11 +134,13 @@ fix-test-report() {
     sed -i'.bak' 's:^\(.*<testcase[^>]* name=\"\)\([^\"]*\)\(\"[^>]* classname=\"[^\"]*\)/\([0-9]*\)\(\".*\)$:\1\2/\4\3\5:g' "$report_file"
     rm -f "$report_file.bak"
 
-    if ! vm-is-macos; then
-        # Protect against invalid XML characters in the CDATA sections
-        sed -i'.bak' 's:[\x00-\x08]::g ; s:\x0B::g ; s:\x0C::g ; s:[\x0E-\x1F]::g' "$report_file"
-        rm -f "$report_file.bak"
+    # Protect against invalid XML characters in the CDATA sections
+    if vm-is-macos; then
+        sed -i'.bak' $'s:[\x00-\x08]::g ; s:\x0B::g ; s:\x0C::g ; s:[\x0E-\x1F]::g' "$report_file"
+    else
+        sed -i'.bak'  's:[\x00-\x08]::g ; s:\x0B::g ; s:\x0C::g ; s:[\x0E-\x1F]::g' "$report_file"
     fi
+    rm -f "$report_file.bak"
 }
 
 
