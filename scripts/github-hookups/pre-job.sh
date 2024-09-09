d4df1379092e57f943fe707c6968bf86cf6d61a1
Paul Baksic <paul.baksic@outlook.fr>
2024-09-09 18:44:19 +0200

    Temporary change to bakpaul owner name

--- scripts/github-hookups/pre-job.sh
+++ scripts/github-hookups/pre-job.sh
@@ -7,7 +7,7 @@ BRANCH_OR_PR_NUMBER=$(echo $GITHUB_REF | awk -F '/' '{print $3}')
 if [ "$REF_TYPE" = "pull" ]; then
     WORK_FOLDER=$GITHUB_WORKSPACE/PR$BRANCH_OR_PR_NUMBER
 else
-    if [ "$GITHUB_REPOSITORY_OWNER" != "sofa-framework" ]; then
+    if [ "$GITHUB_REPOSITORY_OWNER" != "bakpaul" ]; then
         exit 1
     else
         WORK_FOLDER=$GITHUB_WORKSPACE/$BRANCH_OR_PR_NUMBER/$GITHUB_SHA
