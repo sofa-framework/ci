2ee44564d0125d93a97fda2b79158da20d511d25
Paul Baksic <paul.baksic@outlook.fr>
2024-09-09 17:53:16 +0200

    Fix script

--- scripts/github-hookups/pre-job.sh
+++ scripts/github-hookups/pre-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Create folder for this run
-BRANCH_NAME = $(echo $GITHUB_REF | awk -F '/' '{print $NF}')
+BRANCH_NAME=$(echo $GITHUB_REF | awk -F '/' '{print $NF}')
 if [ ! -d $GITHUB_WORKSPACE/$BRANCH_NAME/$GITHUB_SHA ]; then
   mkdir -p $GITHUB_WORKSPACE/$BRANCH_NAME/$GITHUB_SHA;
 fi
