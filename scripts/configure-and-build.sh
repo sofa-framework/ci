5afad01e18530ca27e0dd9263ab8756fa967241a
bakpaul <paul.baksic@outlook.fr>
2025-04-11 14:00:14 +0200

    Add configure-and-build

---
+++ scripts/configure-and-build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: configure-and-build.sh <build-dir> <src-dir> <script-dir> <node-name> <build-type> <config> <python-version> <preset> <generate-binaries> <ci-depends-on-flags>"
+
+if [ "$#" -ge 4 ]; then
+    BUILD_DIR="$(cd "$1" && pwd)"
+    SRC_DIR="$(cd "$2" && pwd)"
+    SCRIPT_DIR="$(cd "$3" && pwd)"
+    NODE_NAME="$4"
+    BUILD_TYPE="$5"
+    CONFIG="$6"
+    PYTHON_VERSION="$7"
+    PRESET="$8"
+    GENERATE_BINARIES="$9"
+    CI_DEPENDS_ON_FLAGS="$10"
+    if [ "$CI_DEPENDS_ON_FLAGS" == "no-ci-depends-on" ]; then
+        CI_DEPENDS_ON_FLAGS=""
+    fi
+else
+    usage; exit 1
+fi
+
+
+# Setup variables for following calls
+
+. ${SCRIPT_DIR}/utils.sh
+NODE_NAME="$(hostname)" # Needed by load-vm-env --> TODO: be able to get hostname multiplatform
+CI_PYTHON3_VERSION=${PYTHON_VERSION} # Needed by load-vm-env, might need to run this inside the docker env
+
+## Setup env variables
+load-vm-env
+
+BUILD_OPTIONS="build-scope-${PRESET}"
+if [ "${GENERATE_BINARIES}" == "true" ]; then
+    BUILD_OPTIONS="$BUILD_OPTIONS build-release-package"
+fi
+
+. ${SCRIPT_DIR}/configure.sh "$BUILD_DIR" "$SRC_DIR" "$CONFIG" "$CI_DEPENDS_ON_FLAGS" "$BUILD_TYPE" "$BUILD_OPTIONS" 
+
+## Call to build 
+${SCRIPT_DIR}/compile.sh "$BUILD_DIR" "$CONFIG" "$BUILD_OPTIONS"
