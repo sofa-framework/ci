576bbbb7ddc0116e497a0fa7623e403d93d03a66
Paul Baksic <30337881+bakpaul@users.noreply.github.com>
2025-05-09 10:57:07 +0200

    Update post-job.sh

--- scripts/github-hookups/post-job.sh
+++ scripts/github-hookups/post-job.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-if [ "$RUNNER_OS" = "Windows" ]; then
-	#Delete simlink
-	echo "Deleting simlink '/c/sl-gha' to reduce path length problem on Windows"
-	cmd //c "rmdir C:\sl-gha" > /dev/null
-fi
+
