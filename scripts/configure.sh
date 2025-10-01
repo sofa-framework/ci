91e8ed8e85dfc58d8bb830fd5fd3662389cd0135
Paul Baksic <paul.baksic@outlook.fr>
2025-10-01 17:39:18 +0200

    Fix cuda

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -392,14 +392,16 @@ elif in-array "build-scope-full" "$BUILD_OPTIONS"; then
     if [[ "$VM_HAS_CUDA" == "true" ]]; then
         add-cmake-option "-DSOFACUDA_DOUBLE=ON"
         if in-array "build-release-package" "$BUILD_OPTIONS"; then
-            add-cmake-option "-DCUDA_ARCH_LIST=6.0;6.1;7.0;7.5;8.0;8.6;8.9"
+            add-cmake-option "-DCMAKE_CUDA_ARCHITECTURES=60;61;70;75;80;86;89"
         else
-            add-cmake-option "-DCUDA_ARCH_LIST=6.0;8.9"
+            add-cmake-option "-DCMAKE_CUDA_ARCHITECTURES=60;89"
         fi
         add-cmake-option "-DPLUGIN_VOLUMETRICRENDERING_CUDA=ON"
         add-cmake-option "-DPLUGIN_SOFADISTANCEGRID_CUDA=ON"
     else
         add-cmake-option "-DPLUGIN_SOFACUDA=OFF"
+        #Deactivate all CUDA modules based on naming convention 'XXX.CUDA" that creates a CMake flag "PLUGIN_XXX_CUDA" thus the double grep 
+     	add-cmake-option "$(cat $SRC_DIR/CMakePresets.json | grep PLUGIN_ | grep _CUDA | awk -F'"' '{ print "-D"$2"=OFF" }' | sort | uniq)"
     fi
 
 fi
